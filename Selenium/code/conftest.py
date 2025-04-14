28574040da9294c4c1d4e2e1f4b5d86f6ec4d2af
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-04-14 12:35:56 +0300

    fix: removed pyorg tests

--- Selenium/code/conftest.py
+++ Selenium/code/conftest.py
@@ -1,4 +1,9 @@
+import pytest
+
 from ui.fixtures import *
+from ui.pages.vk.feed_page import FeedPage
+from ui.pages.vk.main_page import MainPage
+from ui.pages.vk.login_page import LoginPage
 
 
 def pytest_addoption(parser):
@@ -31,3 +36,15 @@ def config(request):
         'selenoid': selenoid,
         'vnc': vnc,
     }
+
+@pytest.fixture
+def main_page(driver):
+    return MainPage(driver=driver)
+
+@pytest.fixture
+def login_page(driver):
+    return LoginPage(driver=driver)
+
+@pytest.fixture
+def feed_page(driver):
+    return FeedPage(driver=driver)
